82b324849e687e04ce6954ea5e3658046eb47c6f
Andreas Alkjær Eriksen <andreas@pop-os.localdomain>
2021-12-13 18:12:33 +0100

    comment

--- script.sh
+++ script.sh
@@ -14,6 +14,7 @@ curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_21.04/R
 # Update
 apt -y --allow-unauthenticated update
 
+# List of packages to be installed
 PACKAGE_LIST=(
 	'grub-efi'
 	'grub2-common'
